42e3d0322c499eacd3b820da70bc842c4d97d241
Philip Newcomer <contact@philipnewcomer.net>
2019-09-24 20:05:51 -0400

    Update readme and "Tested up to" version

--- readme.txt
+++ readme.txt
@@ -1,25 +1,27 @@
 === Show Hidden Post Meta ===
 Contributors: philip.newcomer
-Tags: admin, custom fields, post meta, meta
+Tags: admin, custom fields, meta, post meta
 Requires at least: 3.2
-Tested up to: 3.8
-Stable tag: 1.0
+Tested up to: 5.2
+Stable tag: 1.0.1
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
-Makes hidden custom fields (post meta) visible on the post edit screen
+Makes hidden post meta visible on post edit screens
 
 == Description ==
 
-This is a very simple plugin that will make hidden post meta (also known as custom fields) visible on the post edit screen. There is an older plugin called [Show Hidden Custom Fields](http://wordpress.org/extend/plugins/show-hidden-custom-fields/) which has been abandoned by its author and no longer works since WordPress 3.2, so this plugin was developed to take its place.
+This plugin makes all custom fields (post meta) visible in the "Custom Fields" meta box on post edit screens.
 
 == Installation ==
 
-If you know what custom fields are and why you want to show hidden ones, it is assumed that you also know how to install a plugin.
-
-There are no settings pages or configuration options; simply install and activate the plugin and all hidden custom fields will be made visible.
+Install and activate. There are no settings to configure.
 
 == Changelog ==
+
+= 1.0.1 =
+* Update "Tested up to" version
+* Readme updates
 
 = 1.0 =
 * Initial release
--- show-hidden-post-meta.php
+++ show-hidden-post-meta.php
@@ -1,33 +1,26 @@
 <?php
-/*
-Plugin Name: Show Hidden Post Meta
-Plugin URI: http://wordpress.org/plugins/show-hidden-post-meta/
-Description: Makes hidden custom fields (post meta) visible on the post edit screen
-Author: Philip Newcomer
-Version: 1.0
-Author URI: http://philipnewcomer.net
-License: GPLv2 or later
-License URI: http://www.gnu.org/licenses/gpl-2.0.html
-
-Copyright (C) 2014  Philip Newcomer
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-*/
-
+/**
+ * Plugin Name: Show Hidden Post Meta
+ * Plugin URI: http://wordpress.org/plugins/show-hidden-post-meta/
+ * Description: Makes hidden post meta visible on post edit screens
+ * Version: 1.0.1
+ * Author: Philip Newcomer
+ * Author URI: http://philipnewcomer.net
+ * License: GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * Copyright (C) 2014-2019  Philip Newcomer
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ */
 
 /**
- * Filters is_protected_meta to always show hidden custom fields
+ * Add a filter on `is_protected_meta` to always return false.
  */
 add_filter( 'is_protected_meta', '__return_false', 999 );
